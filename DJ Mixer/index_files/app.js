99f0be6a03564f04a2217edfcbd80044f05b9608
Peter Garrido <peter.garrido57@gmail.com>
2017-05-09 11:22:36 -0500

    plqylist config started

---
+++ DJ Mixer/index_files/app.js
@@ -0,0 +1,18 @@
+// Playlist Config
+
+const djPlayer;
+
+// Folder index to load on page load
+const folderArrayIndex = 0;
+
+// Song on start
+const songsIndexOnStart = new Array();
+
+// Path of Playlist
+const playlistPath = require('./playlist_list.php');
+
+// Path for record API
+const recordPath = require('../djFiles/dj_api_record.php');
+
+// Path for sampler API
+const samplerPath = require('../djFiles/dj_api_sampler.php');
