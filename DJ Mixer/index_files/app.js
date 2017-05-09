4ce42a43ffd13123cc2580778e7fe77764e8c75a
Peter Garrido <peter.garrido57@gmail.com>
2017-05-09 14:00:43 -0500

    display folder js

--- DJ Mixer/index_files/app.js
+++ DJ Mixer/index_files/app.js
@@ -16,3 +16,56 @@ const recordPath = require('../djFiles/dj_api_record.php');
 
 // Path for sampler API
 const samplerPath = require('../djFiles/dj_api_sampler.php');
+
+
+// Init
+const bpmRangeMax = 147;
+const folderArray = new Array();
+const songArray = new Array();
+const mixLoadedOnStart = false;
+
+const lastIdx = -1;
+const randIdx = 0;
+const randList = new Array();
+
+$(document).ready(function() {
+	$.ajaxSetup({ cache: false });
+	djPlayer = getMovieInstance("djPlayer");
+	window.onresize = resizeWindow;
+	resizeWindow();
+	displayFolder();
+});
+
+function getMovieInstance(movieName) {
+	return navigator.appName.indexOf("Microsoft") != -1 && !navigator.appVersion.indexOf('MSIE 9.0') ? window[movieName] : document[movieName;]
+}
+
+function setMenuSelection(selection) {
+	for(let i = 0; i < folderArray.length; i++) document.getElementById('folder'+i).className = "folderList_item";
+	document.getElementById('menu_record').className = "folderList_item";
+	document.getElementById('menu_sampler').className = "folderList_item";
+	document.getElementById('selection').className = "folderList_selected";
+}
+
+// Display folders
+
+function displayFolders() {
+	$('#folderListContent').html("");
+	$.post(playlistPath).success(function(response) {
+		eval("folderArray = " + response);
+		let htmlString = "<table width='100%' border='0' cellpadding='0' cellspacing='0'>";
+
+		for(let i = 0; i < folderArray.length; i++) {
+			let lineStyle = (i % 2 == 0 ? 'folderList_lineStyle1' : 'folderList_lineStyle2');
+			htmlString += "<tr class="+lineStyle+">";
+			htmlString += "<td width='37' align='right'><div class='folderList_icon_folders'>&nbsp;</div></td>";
+			htmlString += "<td align='left'><a id='folder"+i+"' class='folderList_item' onclick=displaySongsInFolder("+i+")>"+folderArray[i].replace("_"," ");+"</a></td>";
+			htmlString += "</tr>";
+		}
+
+		htmlString += "</table>";
+		$('#folderListContent').html(htmlString);
+
+		displaySongsInFolder(folderArrayIndex);
+	});
+}
